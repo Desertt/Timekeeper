8c435d0889212fe67fbc76a76ac1a9a938aa3476
mustafa.dagtekin <mustafa.dagtekin@outlook.com>
2019-07-28 19:08:15 +0300

    Timekeeper

--- Timekeeper/Form1.Designer.cs
+++ Timekeeper/Form1.Designer.cs
@@ -29,12 +29,83 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.button_start = new System.Windows.Forms.Button();
+            this.button_stop = new System.Windows.Forms.Button();
+            this.buttonclear = new System.Windows.Forms.Button();
+            this.label_Watch = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button_start
+            // 
+            this.button_start.Location = new System.Drawing.Point(12, 12);
+            this.button_start.Name = "button_start";
+            this.button_start.Size = new System.Drawing.Size(75, 23);
+            this.button_start.TabIndex = 0;
+            this.button_start.Text = "START";
+            this.button_start.UseVisualStyleBackColor = true;
+            this.button_start.Click += new System.EventHandler(this.button_start_Click);
+            // 
+            // button_stop
+            // 
+            this.button_stop.Location = new System.Drawing.Point(110, 12);
+            this.button_stop.Name = "button_stop";
+            this.button_stop.Size = new System.Drawing.Size(75, 23);
+            this.button_stop.TabIndex = 1;
+            this.button_stop.Text = "STOP";
+            this.button_stop.UseVisualStyleBackColor = true;
+            this.button_stop.Click += new System.EventHandler(this.button_stop_Click);
+            // 
+            // buttonclear
+            // 
+            this.buttonclear.Location = new System.Drawing.Point(205, 12);
+            this.buttonclear.Name = "buttonclear";
+            this.buttonclear.Size = new System.Drawing.Size(75, 23);
+            this.buttonclear.TabIndex = 2;
+            this.buttonclear.Text = "CLEAR";
+            this.buttonclear.UseVisualStyleBackColor = true;
+            this.buttonclear.Click += new System.EventHandler(this.buttonclear_Click);
+            // 
+            // label_Watch
+            // 
+            this.label_Watch.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label_Watch.Font = new System.Drawing.Font("Trebuchet MS", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label_Watch.ForeColor = System.Drawing.Color.Red;
+            this.label_Watch.Location = new System.Drawing.Point(28, 56);
+            this.label_Watch.Name = "label_Watch";
+            this.label_Watch.Size = new System.Drawing.Size(252, 50);
+            this.label_Watch.TabIndex = 3;
+            this.label_Watch.Text = ".";
+            this.label_Watch.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(313, 115);
+            this.Controls.Add(this.label_Watch);
+            this.Controls.Add(this.buttonclear);
+            this.Controls.Add(this.button_stop);
+            this.Controls.Add(this.button_start);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Timekeeper";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button_start;
+        private System.Windows.Forms.Button button_stop;
+        private System.Windows.Forms.Button buttonclear;
+        private System.Windows.Forms.Label label_Watch;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- Timekeeper/Form1.cs
+++ Timekeeper/Form1.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Timekeeper
@@ -15,6 +8,48 @@ namespace Timekeeper
         public Form1()
         {
             InitializeComponent();
+        }
+
+        int saat = 0, dakika = 0, saniye = 0;  //saat dakika ve saniye değişkenlerimizi tanımladık.
+
+        private void buttonclear_Click(object sender, EventArgs e) //Button temizle Click Eventi ile 
+        {
+            timer1.Enabled = false; //Mevctuta timer'ın özlell,iği pasif ediliyor.Değişkenler 0'a çekiliyor.
+            saat = 0;
+            dakika = 0;
+            saniye = 0;
+            label_Watch.Text = Convert.ToString("0:0:0");  //Lable'ı  ("0:0:0") formatına çektik.
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label_Watch.Text = ((Convert.ToString(saniye) + ":") + (Convert.ToString(dakika) + ":") + (Convert.ToString(saat) + " "));
+            /*Label'a değer yazdırmak için değişkenleri string'e çevirdik*/
+            if (saniye == 59) //her 59.saniyeye gelindiğinde /*saniye 0,dakika 1 artacak -- */
+            {
+                saniye = 0;
+                dakika = dakika + 1;
+                if (dakika == 60) /*eğer dakika 60 ise  saniye 0,dakika 0 , saat 1 artacak*/
+                {
+                    saniye = 0;
+                    dakika = 0;
+                    saat = saat + 1;
+
+                }
+
+            }
+            saniye = saniye + 1; //saniye her daim 1 artacak-Burada timer'ın Interval değeri 1000ms=1 sn  olarak seçildi.
+
+        }
+
+        private void button_stop_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false; //burada timer devre dışı
+        }
+
+        private void button_start_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true; //timer çalışsın
         }
     }
 }
